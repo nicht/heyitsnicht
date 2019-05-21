fbe89c9dbf26dfe9c76e8291794a7ed2213a665a
Rodolfo Olivieri nicht <rodolfo.olivieri3@gmail.com>
2019-05-21 13:18:11 -0300

    working on that shinny new CLI

--- source/cli.js
+++ source/cli.js
@@ -1,25 +1,18 @@
 #!/usr/bin/env node
-'use strict';
-const path = require('path');
-const meow = require('meow');
-const termImg = require('term-img');
-const terminalImage = require('terminal-image');
-const importJsx = require('import-jsx');
-const React = require('react');
-const {render} = require('ink');
+"use strict";
+const path = require("path");
+const meow = require("meow");
+const termImg = require("term-img");
+const terminalImage = require("terminal-image");
+const importJsx = require("import-jsx");
+const React = require("react");
+const { render } = require("ink");
 
-const ui = importJsx('./ui');
+const ui = importJsx("./ui");
 
 meow(`
 	Usage
-	  $ sindresorhus
+	  $ heyitsnicht
 `);
-
-const fallback = async () => {
-	const image = await terminalImage.file(path.join(__dirname, 'avatar-fallback.png'));
-	console.log(image);
-};
-
-termImg(path.join(__dirname, 'avatar.png'), {fallback});
 
 render(React.createElement(ui));
--- source/ui.js
+++ source/ui.js
@@ -1,10 +1,11 @@
-'use strict';
-const path = require('path');
-const React = require('react');
-const {Box, Text} = require('ink');
-const SelectInput = require('ink-select-input').default;
-const open = require('open');
-const terminalImage = require('terminal-image');
+"use strict";
+const path = require("path");
+const React = require("react");
+const { Box, Text } = require("ink");
+const SelectInput = require("ink-select-input").default;
+const open = require("open");
+const Gradient = require("ink-gradient");
+const BigText = require("ink-big-text");
 
 const handleSelect = item => {
 	if (item.url) {
@@ -26,47 +27,53 @@ const createItems = items => {
 
 const items = createItems([
 	{
-		label: 'Website',
-		url: 'https://sindresorhus.com'
+		label: "Website",
+		url: "https://sindresorhus.com"
 	},
 	{
-		label: 'Twitter',
-		url: 'https://twitter.com/sindresorhus'
+		label: "Twitter",
+		url: "https://twitter.com/sindresorhus"
 	},
 	{
-		label: 'GitHub',
-		url: 'https://github.com/sindresorhus'
+		label: "GitHub",
+		url: "https://github.com/sindresorhus"
 	},
 	{
-		label: 'Blog',
-		url: 'https://blog.sindresorhus.com'
+		label: "Blog",
+		url: "https://blog.sindresorhus.com"
 	},
 	{
-		label: 'Ask Me Anything',
-		url: 'https://github.com/sindresorhus/ama'
+		label: "Ask Me Anything",
+		url: "https://github.com/sindresorhus/ama"
 	},
 	{
-		label: 'Contact',
-		url: 'https://sindresorhus.com/contact'
+		label: "Contact",
+		url: "https://sindresorhus.com/contact"
 	},
 	{
-		label: 'Support my open source work',
-		url: 'https://www.patreon.com/sindresorhus'
+		label: "Support my open source work",
+		url: "https://www.patreon.com/sindresorhus"
 	},
 	{
-		label: 'Unicorns!',
+		label: "Unicorns!",
 		async action() {
-			console.log(await terminalImage.file(path.join(__dirname, 'unicorn1.gif')));
-			console.log(await terminalImage.file(path.join(__dirname, 'unicorn2.gif')));
-			console.log(await terminalImage.file(path.join(__dirname, 'unicorn3.gif')));
+			console.log(
+				await terminalImage.file(path.join(__dirname, "unicorn1.gif"))
+			);
+			console.log(
+				await terminalImage.file(path.join(__dirname, "unicorn2.gif"))
+			);
+			console.log(
+				await terminalImage.file(path.join(__dirname, "unicorn3.gif"))
+			);
 		}
 	},
 	// TODO: Add separator item here when https://github.com/vadimdemedes/ink-select-input/issues/4 is done
 	{
-		label: '---------'
+		label: "---------"
 	},
 	{
-		label: 'Quit',
+		label: "Quit",
 		action() {
 			process.exit(); // eslint-disable-line unicorn/no-process-exit
 		}
@@ -75,9 +82,14 @@ const items = createItems([
 
 module.exports = () => (
 	<Box flexDirection="column">
+		<Gradient name="rainbow">
+			<BigText text="hey! it's me, nicht!" />
+		</Gradient>
 		<Box marginBottom={1}>
-			<Text>I’m a full-time open-sourcerer making things like macOS apps, CLI tools, and modules.</Text>
+			<Text>
+				Meu nome é Rodolfo Olivieri e sou Desenvolvedor FullStack (NodeJS e PHP)
+			</Text>
 		</Box>
-		<SelectInput items={items} onSelect={handleSelect}/>
+		<SelectInput items={items} onSelect={handleSelect} />
 	</Box>
 );
